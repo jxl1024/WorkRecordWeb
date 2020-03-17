57a022893a2192e0f3921648fe727bdda267b542
liuna <1755631124@qq.com>
2020-03-17 17:11:34 +0800

    测试

--- workrecordweb-project/src/views/Home.vue
+++ workrecordweb-project/src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" test="我测试一下" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
